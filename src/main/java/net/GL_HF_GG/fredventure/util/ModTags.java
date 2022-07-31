ba7820fc3ea7b318c5f26339cb7ea3d2aff90a92
leeeveee <109473326+leeeveee@users.noreply.github.com>
2022-07-31 17:32:03 +0200

    veel

--- src/main/java/net/GL_HF_GG/fredventure/util/ModTags.java
+++ src/main/java/net/GL_HF_GG/fredventure/util/ModTags.java
@@ -12,19 +12,19 @@ import net.minecraftforge.common.Tags;
 public class ModTags {
     public static class Blocks {
 
-
-    private static TagKey<Block> tag(String name){
+    public static final TagKey<Block> WOODEN_FENCE_GATE = tag("wooden_fence_gate");
+    public static  TagKey<Block> tag(String name){
         return BlockTags.create(new ResourceLocation(fredventure.MOD_ID, name));
     }
-    private static TagKey<Block> forgeTag(String name){
+    public static TagKey<Block> forgeTag(String name){
         return BlockTags.create(new ResourceLocation("forge", name));
     }
     }
     public static class Items{
-        private static TagKey<Item> tag(String name){
+        public static TagKey<Item> tag(String name){
             return ItemTags.create(new ResourceLocation(fredventure.MOD_ID, name));
         }
-        private static TagKey<Item> forgeTag(String name){
+        public static TagKey<Item> forgeTag(String name){
             return ItemTags.create(new ResourceLocation("forge", name));
         }
     }
