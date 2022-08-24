bb194b0c88b96cce54cae12cf8c975fa386d6b8c
leeeveee <109473326+leeeveee@users.noreply.github.com>
2022-08-24 20:32:02 +0200

    veel

---
+++ src/main/java/net/GL_HF_GG/fredventure/item/costum/ModFlammableRotatedPillarBlock.java
@@ -0,0 +1,49 @@
+package net.GL_HF_GG.fredventure.item.costum;
+
+import net.GL_HF_GG.fredventure.block.ModBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ToolAction;
+
+import javax.annotation.Nullable;
+
+import static net.minecraft.world.level.levelgen.SurfaceRules.state;
+
+public class ModFlammableRotatedPillarBlock extends RotatedPillarBlock {
+    public ModFlammableRotatedPillarBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public boolean isFlammable(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+        return true;
+    }
+
+    @Override
+    public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+        return 5;
+    }
+
+    @Override
+    public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+        return 5;
+    }
+
+    @Nullable
+    @Override
+    public BlockState getToolModifiedState(BlockState state, UseOnContext context, ToolAction toolAction, boolean simulate) {
+        if(context.getItemInHand().getItem() instanceof AxeItem) {
+            if(state.is(ModBlocks.BLUE_LOG.get())) {
+                return ModBlocks.STRIPPED_BLUE_LOG.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+            }
+
+        }
+
+        return super.getToolModifiedState(state, context, toolAction, simulate);
+    }
+}
